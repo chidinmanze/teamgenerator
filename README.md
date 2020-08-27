a4525c5ac64df46db6d6e466e43e3a24814ce396
Chidinma Nze <chidinma.ogbuaku@gmail.com>
2020-08-27 06:09:37 -0400

    updated working app and readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# teamgenerator
+# Team Generator
+Team Generator is a Node.js application that allows a user to create an web page with team member profiles after answering a series of Inquirer prompts.
+
+## Getting Started
+To get the app running, install Inquirer after ensuring that your terminal is in the correct directory. To do so, type the following command into the terminal:
+
+npm install inquirer
+
+This will trigger a series of prompts that allows you to enter information for various members of a team. Team role options are:
+- Manager
+- Engineer
+- Intern
+
+After entering all information, a team.html file will be generated into the output folder. Open this file in the browser, and you'll see the team members you created in the terminal. 
+
+## Demo
+<img src="./TeamProflieDemo.gif" alt="app demo" style="width:700px;height:600px;">
--- app.js
+++ app.js
@@ -16,216 +16,193 @@ const teamArray = [];
 // and to create objects for each team member (using the correct classes as blueprints!)
 
 function first() {
-    const questions = inquirer.prompt([
-        {
-          type: "list",
-          choices: ["Manager", "Engineer", "Intern"],
-          message: "What is your role?",
-          name: "role"
-        }
-     
-            
-]).then(function(data){
-
-    if(data.role === "Manager") {
-         managerInfo()
-    }else if(data.role === "Engineer") {
-        engineerInfo() 
-    }else if(data.role === "Intern") {
-        internInfo()
-    }
-})
-
+  const questions = inquirer.prompt([
+      {
+        type: "list",
+        choices: ["Manager", "Engineer", "Intern"],
+        message: "What is your role?",
+        name: "role",
+      },
+    ])
+    .then(function (data) {
+      if (data.role === "Manager") {
+        managerInfo();
+      } else if (data.role === "Engineer") {
+        engineerInfo();
+      } else if (data.role === "Intern") {
+        internInfo();
+      }
+    });
+}
 
 function managerInfo() {
-    inquirer.prompt([
+    inquirer
+      .prompt([
         {
-            type: "input",
-            message: "What is your name?",
-            name: "name"
-          },
-    
-          {
-            type: "input",
-            message: "What is your id?",
-            name: "id"
-          },
-    
+          type: "input",
+          message: "What is your name?",
+          name: "name",
+        },
+
+        {
+          type: "input",
+          message: "What is your id?",
+          name: "id",
+        },
+
         {
           type: "input",
           message: "What is your email address?",
-          name: "email"
+          name: "email",
         },
-    
-        {
-            type: "input",
-            message: "What is your office phone number?",
-            name: "officeNumber"
-          },
 
-          {
-            type: "list",
-            choices: ["Yes", "No"],
-            message: "Would you like to add another team member?",
-            name: "continue"
+        {
+          type: "input",
+          message: "What is your office phone number?",
+          name: "officeNumber",
+        },
+
+        {
+          type: "list",
+          choices: ["Yes", "No"],
+          message: "Would you like to add another team member?",
+          name: "continue",
+        },
+      ])
+      .then(function (data) {
+        const employee = new Manager(data.name, data.id, data.email, data.officeNumber)
+        teamArray.push(employee);
+
+        console.log(teamArray)
+        if (data.continue === "Yes") {
+          first();
+        } else if (data.continue === "No") {
+          fs.writeFile("./output/team.html", render(teamArray), (err) => { 
+              if (err) 
+                console.log(err); 
+              else { 
+                console.log("File written successfully\n");
+              }
         }
-    
-        ]).then(function(data) {
-         
-            teamArray.push({
-                "Name": data.name,
-                "Role": "Manager",
-                "Email": data.email,
-                "ID": data.id,
-                "office number": data.officeNumber
-            })
-                      
-            if (data.continue === "Yes") {
-                first()
-            }else if(data.continue === "No") {
-                return
-            }
+      
+        )}
+      })
+  }
 
-        });
-
-       
-    };
-    
-    function engineerInfo() {
-    inquirer.prompt([
+  function engineerInfo() {
+    inquirer
+      .prompt([
         {
-            type: "input",
-            message: "What is your name?",
-            name: "name"
-          },
-    
-          {
-            type: "input",
-            message: "What is your id?",
-            name: "id"
-          },
-    
+          type: "input",
+          message: "What is your name?",
+          name: "name",
+        },
+
+        {
+          type: "input",
+          message: "What is your id?",
+          name: "id",
+        },
+
         {
           type: "input",
           message: "What is your email address?",
-          name: "email"
+          name: "email",
         },
-    
-        
-    { 
-       type: "input",
-       message: "What is your Git Hub user name?",
-       name: "github"
-    },
 
-    {
-        type: "list",
-        choices: ["Yes", "No"],
-        message: "Would you like to add another team member?",
-        name: "continue"
-    }
-    
-    
-    ]) .then(function(data){
-       
-        teamArray.push({
-            "Name": data.name,
-            "Role": "Engineer",
-            "Email": data.email,
-            "ID": data.id,
-            "GitHub": data.github
-        })
-                     
-        if(data.continue === "Yes") {
-             first()
-        }else if(data.continue === "No") {
-            return
+        {
+          type: "input",
+          message: "What is your Git Hub user name?",
+          name: "github",
+        },
+
+        {
+          type: "list",
+          choices: ["Yes", "No"],
+          message: "Would you like to add another team member?",
+          name: "continue",
+        },
+      ])
+      .then(function (data) {
+        const employee = new Engineer (data.name, data.id, data.email, data.github )
+        teamArray.push(employee);
+
+        console.log(teamArray)
+
+        if (data.continue === "Yes") {
+          first();
+        } else if (data.continue === "No") {
+          fs.writeFile("./output/team.html", render(teamArray), (err) => { 
+              if (err) 
+                console.log(err); 
+              else { 
+                console.log("File written successfully\n");
+              }
         }
-    });
-};
+      
+        )}
+      })
+  };
 
+  function internInfo() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is your name?",
+          name: "name",
+        },
 
-    function internInfo() {
-        inquirer.prompt([
+        {
+          type: "input",
+          message: "What is your id?",
+          name: "id",
+        },
 
-            {
-                type: "input",
-                message: "What is your name?",
-                name: "name",
-              },
-        
-              {
-                type: "input",
-                message: "What is your id?",
-                name: "id",
-              },
-        
-            {
-              type: "input",
-              message: "What is your email address?",
-              name: "email",
-            },
-        
+        {
+          type: "input",
+          message: "What is your email address?",
+          name: "email",
+        },
 
-            {
-                type: "input",
-                message: "What school do you attend?",
-                name: "school"
-            },
+        {
+          type: "input",
+          message: "What school do you attend?",
+          name: "school",
+        },
 
-            {
-                type: "list",
-                choices: ["Yes", "No"],
-                message: "Would you like to add another team member?",
-                name: "continue"
-            }
-
-        ]).then(function(data){
-          
-            teamArray.push({
-                "Name": data.name,
-                "Role": "Intern",
-                "Email": data.email,
-                "ID": data.id,
-                "School": data.school
-            })
-            
-                        
-            if(data.continue === "Yes") {
-                 first()
-            }else if(data.continue === "No") {
-               return
-            }
-        });   
+        {
+          type: "list",
+          choices: ["Yes", "No"],
+          message: "Would you like to add another team member?",
+          name: "continue"
+        }
+      
+      ])
+      .then(function (data) {
+       
+        const employee = new Intern (data.name, data.id, data.email, data.school)
+            teamArray.push(employee);
     
-
-    };
-
+            console.log(teamArray)
+    
         
-};
 
-first();
+        if (data.continue === "Yes") {
+          first();
+        } else if (data.continue === "No") {
+          fs.writeFile("./output/team.html", render(teamArray), (err) => { 
+              if (err) 
+                console.log(err); 
+              else { 
+                console.log("File written successfully\n");
+              }
+        }
+      
+        )}
+  })
+  };
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+      
 
- render(teamArray);
-
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.,
-fs.writeSToFile("team.html", render(teamArray), utf8);
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+first()
